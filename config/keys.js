1f5ca39ac7fec686d7b2c370f20ba83162679470
Tomen1980 <53292050+Tomen1980@users.noreply.github.com>
2020-04-21 02:13:53 +0700

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://agungjumantoroandrian:'+ encodeURIComponent('agung111') + '@test-cluster1-5kauf.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
